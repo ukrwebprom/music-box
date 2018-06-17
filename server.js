74a8f8cfc48e3c406a32b7692bd0b38d2f89ed66
ukrwebprom <ukrwebprom@gmail.com>
2018-06-17 22:19:49 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var http = require("http");
+var url = require("url");
+
+function start(route, handle){
+	var server = http.createServer(
+	function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response);
+	});
+server.listen(8888);
+}
+exports.start = start;
